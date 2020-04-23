3667b6aad83dc998f196dcf8ee4c13c509b34522
Thach Luu <luuphanbaothach@gmail.com>
2020-04-23 07:29:04 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,33 @@
 # Add your code here
+class Dog 
+
+    attr_reader :name
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save 
+    end 
+
+    def save
+        @@all << self 
+    end 
+
+    def self.all
+        @@all
+    end 
+
+    def self.clear_all
+        @@all.clear
+    end 
+
+    def self.print_all
+         my_array = @@all.map do |dog|
+            dog.name
+        end 
+        puts my_array 
+    end 
+        
+    
+
+end
